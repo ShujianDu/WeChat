5a97be27d58f0e0e80fa370604bb02b91d413c92
zqd <190167177@qq.com>
2016-03-17 20:03:47 +0800

    修改输入输出

--- SystemAdapterService/src/main/scala/com/yada/system/adapter/gcs/GCSService.scala
+++ SystemAdapterService/src/main/scala/com/yada/system/adapter/gcs/GCSService.scala
@@ -15,6 +15,19 @@ trait GCSService {
   def getBalance(sessionID: String, channelID: String, cardNos: List[String]): List[GCSBalance]
 
   /**
+    * 根据卡号查询额度
+    * 1、根据卡号查询币种
+    * 2、根据卡号和币种查询额度
+    *
+    * @param sessionID gcsSessionId
+    * @param channelID 渠道编号
+    * @param cardNo    卡号
+    * @return GCSBalance
+    */
+  //TODO JFM
+  def getBalance(sessionID: String, channelID: String, cardNo: String): GCSBalance
+
+  /**
     * 查账单周期
     *
     * @param sessionId gcsSessionId
@@ -22,6 +35,7 @@ trait GCSService {
     * @param cardNo    卡号
     * @return
     */
+  //TODO JFM
   def getBillingPeriods(sessionId: String, channelId: String, cardNo: String): List[GCSBillingPeriods]
 
   /**
@@ -33,6 +47,7 @@ trait GCSService {
     * @param accountId   账户ID
     * @return
     */
+  //TODO JFM
   def getBillingSummary(sessionId: String, channelId: String, statementNo: String, accountId: String): GCSBillingSummary
 
   /**
@@ -63,7 +78,15 @@ trait GCSService {
   def getCurrencyCodes(sessionId: String, channelId: String, cardNo: String): List[String]
 
 
-  def getCardCurrencyCodeAndStyle(sessionId: String, channelId: String, cardNo: String)
+  /**
+    * 查询卡的币种和产品类型
+    *
+    * @param sessionId gcsSessionId
+    * @param channelId 渠道编号
+    * @param cardNo    卡号
+    * @return (币种列表,卡片产品类型)
+    */
+  def getCardCurrencyCodeAndStyle(sessionId: String, channelId: String, cardNo: String): (List[String], String)
 
   /**
     * 账单寄送方式查询
@@ -199,7 +222,7 @@ trait GCSService {
     *
     * @param sessionId gcsSessionId
     * @param channelId 渠道编号
-    * @param cardNo 卡号
+    * @param cardNo    卡号
     * @return 虚拟卡卡号列表
     */
   def getVirtualCards(sessionId: String, channelId: String, cardNo: String): List[String]
@@ -207,55 +230,52 @@ trait GCSService {
   /**
     * 临时提额评测
     *
+    * 报文默认值:
+    * AT5605 - 任务类型标识 - "2临增"
+    * AT5602 - 渠道系统标识 - "06微信"
+    *
     * @param sessionId   gcsSessionId
     * @param channelId   渠道编号
     * @param certType    客户证件类型
     * @param certNum     客户证件号码
     * @param phoneNumber 手机号
     * @param cardNo      卡号
-    * @return 临时提升评测结果
+    * @param currencyNo  币种
+    * @return 临时提额评测结果
     */
   def creditLimitTemporaryUpReview(sessionId: String, channelId: String, certType: String, certNum: String,
-                                   phoneNumber: String, cardNo: String): GCSCreditLimitTemporaryUpReviewResult
+                                   phoneNumber: String, cardNo: String, currencyNo: String): GCSCreditLimitTemporaryUpReviewResult
 
   /**
     * 临时提额提交
     *
-    * @param sessionId         gcsSessionId
-    * @param channelId         渠道编号
-    * @param eosID             工单ID
-    * @param eosCustomerName   客户姓名
-    * @param eosCustomerIdType 客户证件类型
-    * @param certNum           客户证件号码
-    * @param phoneNumber       手机号
-    * @param cardNo            卡号
-    * @param eosPreAddLimit    客户期望额度
-    * @param eosStarLimitDate  增额生效开始日期
-    * @param eosEndlimitdate   增额生效结束日期
-    * @param cardStyle         产品类型
-    * @param issuingBranchId   发卡行号
-    * @param pmtCreditLimit    当前卡的永久额度
-    * @return 临时提升评测结果
+    * 报文默认值:
+    * eosID - 工单ID - 固定规则生成
+    * eosType - 工单类型  - "02临增"
+    * eosReason - 增额原因 - "01客户主动"
+    * eosEmergencyDegree - 紧急程度 - "2：中（紧急）"
+    * eosCustomerType - 客户预设额度类型 - "1-增额类"
+    * eosRequestPath - 工单渠道标示 - "06微信"
+    *
+    * @param sessionId                  gcsSessionId
+    * @param channelId                  渠道编号
+    * @param gcsTemporaryUpCommitParams 临时提额授权参数
+    * @return 临时提额授权结果
     */
-  def temporaryUpCommit(sessionId: String, channelId: String, eosID: String, eosCustomerName: String, eosCustomerIdType: String,
-                        certNum: String, phoneNumber: String, cardNo: String, eosPreAddLimit: String, eosStarLimitDate: String,
-                        eosEndlimitdate: String, cardStyle: String, issuingBranchId: String, pmtCreditLimit: String): Boolean
+  def temporaryUpCommit(sessionId: String, channelId: String, gcsTemporaryUpCommitParams: GCSTemporaryUpCommitParams): Boolean
 
   /**
     * 信用卡额度临时提额调整状态查询
     *
     * @param sessionId gcsSessionId
     * @param channelId 渠道编号
-    * @param eosIdtype 证件类型
+    * @param eosIDType 证件类型
     * @param idNumber  证件号码
     * @param cardNo    卡号
     * @param eosId     工作单ID
-    * @return 信用卡额度临时提升调整状态列表
+    * @return 信用卡额度临时提额调整状态列表
     */
-  def getTemporaryUpCommitStatus(sessionId: String, channelId: String, eosIdtype: String, idNumber: String, cardNo: String, eosId: String): List[GCSCreditLimitTemporaryUpStatus]
-
-  //TODO 卡户分期接口存在疑问！ 根据卡号或者证件号查询客户信息及卡信息存在疑问
-  //  def getAccountSummaryInfo
+  def getTemporaryUpCommitStatus(sessionId: String, channelId: String, eosIDType: String, idNumber: String, cardNo: String, eosId: String): List[GCSCreditLimitTemporaryUpStatus]
 
   /**
     * 查询所有可分期的消费交易
@@ -279,13 +299,15 @@ trait GCSService {
     *
     * @param sessionId gcsSessionId
     * @param channelId 渠道编号
-    * @param parms     参数实体
-    * @return
+    * @param params    参数实体
+    * @return 消费分期试算结果
     */
-  def costConsumptionInstallment(sessionId: String, channelId: String, parms: GCSConsumptionInstallmentParms): GCSConsumptionInstallmentCost
+  def costConsumptionInstallment(sessionId: String, channelId: String, params: GCSConsumptionInstallmentParams): GCSConsumptionInstallmentResult
 
   /**
     * 消费分期授权
+    *
+    * 报文默认值:
     * transactionNo - “3110”( POS消费还原)
     * mcc -“5311”
     * installmentPlan - “EP01”
@@ -293,24 +315,59 @@ trait GCSService {
     *
     * @param sessionId gcsSessionId
     * @param channelId 渠道编号
-    * @param parms     参数实体
+    * @param params    参数实体
     * @return GCS返回码
     */
-  def authorizationConsumptionInstallment(sessionId: String, channelId: String, parms: GCSConsumptionInstallmentParms): String
+  def authorizationConsumptionInstallment(sessionId: String, channelId: String, params: GCSConsumptionInstallmentParams): String
 
+
+  /**
+    * 获取账单金额上下限
+    * 1、根据卡号、币种查询查询账户ID
+    * 2、根据账户ID、币种查询账单分期金额上下限
+    *
+    * @param sessionId    gcsSessionId
+    * @param channelId    渠道编号
+    * @param cardNo       卡号
+    * @param currencyCode 币种
+    * @return 账单金额上下限结果
+    */
+  def getAmountLimit(sessionId: String, channelId: String, cardNo: String, currencyCode: String): GCSAmountLimit
+
+  /**
+    * 账单分期费用试算
+    *
+    * @param sessionId                gcsSessionId
+    * @param channelId                渠道编号
+    * @param gcsBillInstallmentParams 账单分期费用试算参数
+    * @return 账单分期费用试算结果
+    */
+  def queryBillCost(sessionId: String, channelId: String, gcsBillInstallmentParams: GCSBillInstallmentParams): GCSBillInstallmentResult
+
+  /**
+    * 账单分期授权
+    *
+    * @param sessionId                gcsSessionId
+    * @param channelId                渠道编号
+    * @param gcsBillInstallmentParams 账单分期授权参数
+    * @return GCS返回码
+    */
+  def billInstallment(sessionId: String, channelId: String, gcsBillInstallmentParams: GCSBillInstallmentParams): String
 }
 
 /**
+  * 额度
   *
   * @param cardNo                    卡号
   * @param currencyCode              币种
   * @param limitCount                总授信额度
-  * @param avaliableCount            总可用额
+  * @param availableCount            总可用额
   * @param preCashAdvanceCreditLimit 取现可用额度
   */
-case class GCSBalance(cardNo: String, currencyCode: String, limitCount: String, avaliableCount: String, preCashAdvanceCreditLimit: String)
+case class GCSBalance(cardNo: String, currencyCode: String, limitCount: String, availableCount: String, preCashAdvanceCreditLimit: String)
 
 /**
+  * 账单周期
   *
   * @param accountId       账户ID
   * @param currencyCode    币种
@@ -321,6 +378,7 @@ case class GCSBalance(cardNo: String, currencyCode: String, limitCount: String, 
 case class GCSBillingPeriods(accountId: String, currencyCode: String, periodStartDate: String, periodEndDate: String, statementNo: String)
 
 /**
+  * 账单摘要
   *
   * @param periodStartDate  账期开始日期
   * @param periodEndDate    账期结束日期/账单日期
@@ -333,6 +391,7 @@ case class GCSBillingPeriods(accountId: String, currencyCode: String, periodStar
 case class GCSBillingSummary(periodStartDate: String, periodEndDate: String, paymentDueDate: String, closingBalance: String, currencyCode: String, minPaymentAmount: String)
 
 /**
+  * 账单明细
   *
   * @param cardNo                 卡号
   * @param currencyCode           币种
@@ -345,6 +404,7 @@ case class GCSBillingSummary(periodStartDate: String, periodEndDate: String, pay
 case class GCSBillingDetail(cardNo: String, currencyCode: String, transactionDate: String, transactionAmount: String, transactionDescription: String, debitCreditCode: String)
 
 /**
+  * 账单寄送方式
   *
   * @param cardNo       卡号
   * @param billSendType 账单寄送方式
@@ -352,6 +412,7 @@ case class GCSBillingDetail(cardNo: String, currencyCode: String, transactionDat
 case class GCSBillSendType(cardNo: String, billSendType: String)
 
 /**
+  * 持卡人信息
   *
   * @param familyName 客户姓
   * @param firstName  客户名
@@ -361,6 +422,7 @@ case class GCSBillSendType(cardNo: String, billSendType: String)
 case class GCSCardHolderInfo(familyName: String, firstName: String, gender: String, mobileNo: String)
 
 /**
+  * 临时提额评测结果
   *
   * @param approve         授权是否批准
   * @param amount          建议额度
@@ -372,6 +434,7 @@ case class GCSCardHolderInfo(familyName: String, firstName: String, gender: Stri
 case class GCSCreditLimitTemporaryUpReviewResult(approve: String, amount: String, cardStyle: String, issuingBranchId: String, creditLimit: String, pmtCreditLimit: String)
 
 /**
+  * 信用卡额度临时提额调整状态实体
   *
   * @param eosId            工单ID
   * @param eosState         工单状态 70-审批批准并已同步CSR、0-待分配、0-待重新分配、20-待领取、30-审批中、40-转上级审批中、
@@ -384,6 +447,8 @@ case class GCSCreditLimitTemporaryUpReviewResult(approve: String, amount: String
 case class GCSCreditLimitTemporaryUpStatus(eosId: String, eosState: String, eosImpTime: String, eosLimit: String, eosStarLimitDate: String, eosEndLimitDate: String)
 
 /**
+  * 可分期的消费交易
+  *
   * @param cardNo                 卡号
   * @param transactionDate        交易日期
   * @param transactionAmount      交易金额
@@ -397,6 +462,7 @@ case class GCSConsumptionInstallmentsEntity(cardNo: String, transactionDate: Str
                                             accountID: String, accountedID: String, accountNoID: String)
 
 /**
+  * 消费分期结果
   *
   * @param cardNo                      卡号
   * @param installmentAmount           分期金额
@@ -407,10 +473,12 @@ case class GCSConsumptionInstallmentsEntity(cardNo: String, transactionDate: Str
   * @param installmentsNumber          分期期数
   * @param currencyCode                币种
   */
-case class GCSConsumptionInstallmentCost(cardNo: String, installmentAmount: String, installmentFee: String, installmentsAlsoAmountFirst: String, installmentsAlsoAmountEach: String,
-                                         billFeeMeans: String, installmentsNumber: String, currencyCode: String)
+case class GCSConsumptionInstallmentResult(cardNo: String, installmentAmount: String, installmentFee: String, installmentsAlsoAmountFirst: String, installmentsAlsoAmountEach: String,
+                                           billFeeMeans: String, installmentsNumber: String, currencyCode: String)
 
 /**
+  * 消费分期参数
+  *
   * @param accountKeyOne      帐户键值1--原消费中账户ID
   * @param accountKeyTwo      帐户键值2--原消费中入账账户ID
   * @param currencyCode       币种--原消费币种
@@ -421,6 +489,66 @@ case class GCSConsumptionInstallmentCost(cardNo: String, installmentAmount: Stri
   * @param installmentPeriods 分期付款期数
   * @param isfeeFlag          是否分期收取手续费
   */
-case class GCSConsumptionInstallmentParms(accountKeyOne: String, accountKeyTwo: String, currencyCode: String,
-                                          billDateNo: String, transactionNo: String, transactionAmount: String, cardNo: String, accountNoId: String,
-                                          installmentPeriods: String, isfeeFlag: String)
+case class GCSConsumptionInstallmentParams(accountKeyOne: String, accountKeyTwo: String, currencyCode: String,
+                                           billDateNo: String, transactionNo: String, transactionAmount: String, cardNo: String, accountNoId: String,
+                                           installmentPeriods: String, isfeeFlag: String)
+
+/**
+  * 账单分期上下限结果
+  *
+  * @param currencyCode 币种
+  * @param minAmount    账单分期金额下限真实
+  * @param maxAmount    账单分期金额上限
+  * @param respCode     返回码
+  */
+case class GCSAmountLimit(currencyCode: String, minAmount: String, showMinAmount: String, maxAmount: String, respCode: String)
+
+/**
+  * 账单分期参数
+  * 用于账单分期试算和账单分期授权2个接口中
+  *
+  * @param accountId           账户ID
+  * @param accountNumber       帐户号码(使用TS011006查询后的“accountNo”域值)
+  * @param currencyCode        币种
+  * @param billLowerAmount     账单分期下限金额
+  * @param billActualAmount    账单分期实际金额
+  * @param installmentPlanID   分期计划ID
+  * @param installmentsNumber  分期期数
+  * @param feeInstallmentsFlag 手续费分期标识(1---标识手续费分期收取，0---标识手续费一次性收取)
+  */
+case class GCSBillInstallmentParams(accountId: String, accountNumber: String, currencyCode: String, billLowerAmount: String,
+                                    billActualAmount: String, installmentPlanID: String, installmentsNumber: String, feeInstallmentsFlag: String)
+
+/**
+  * 账单分期费用试算结果
+  *
+  * @param currentBillMinimum          本期账单最小还款额
+  * @param installmentsfee             分期手续费
+  * @param installmentsAlsoAmountFirst 分期后每期应还金额-首期
+  * @param installmentsAlsoAmountEach  分期后每期应还金额-后每期
+  * @param currentBillSurplusAmount    本期账单剩余还款金额
+  * @param billFeeMeans                分期手续费收取方式
+  * @param installmentsNumber          分期期数
+  */
+case class GCSBillInstallmentResult(currentBillMinimum: String, installmentsfee: String, installmentsAlsoAmountFirst: String, installmentsAlsoAmountEach: String,
+                                    currentBillSurplusAmount: String, billFeeMeans: String, installmentsNumber: String)
+
+/**
+  * 临时提额授权参数
+  *
+  * @param eosCustomerName   客户姓名
+  * @param eosCustomerIdType 客户证件类型
+  * @param certNum           客户证件号码
+  * @param phoneNumber       手机号
+  * @param cardNo            卡号
+  * @param eosCurrency       币种
+  * @param eosPreAddLimit    客户期望额度
+  * @param eosStarLimitDate  增额生效开始日期
+  * @param eosEndLimitDate   增额生效结束日期
+  * @param cardStyle         产品类型 - 评测接口的返回结果
+  * @param issuingBranchId   发卡行号 - 评测接口的返回结果
+  * @param pmtCreditLimit    当前卡的永久额度 -评测接口的返回结果
+  */
+case class GCSTemporaryUpCommitParams(eosCustomerName: String, eosCustomerIdType: String,
+                                      certNum: String, phoneNumber: String, cardNo: String, eosCurrency: String, eosPreAddLimit: String, eosStarLimitDate: String,
+                                      eosEndLimitDate: String, cardStyle: String, issuingBranchId: String, pmtCreditLimit: String)
