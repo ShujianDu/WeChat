ce064d6a9c0b4e2158202e579a67e1c3b4d9b90e
fengming.jiang@bjyada.com <fengming.jiang@bjyada.com>
2016-03-17 14:23:28 +0800

    补充GCS基本实现

--- SystemAdapterService/src/main/scala/com/yada/sdk/gcs/Gateway.scala
+++ SystemAdapterService/src/main/scala/com/yada/sdk/gcs/Gateway.scala
@@ -1,17 +1,17 @@
-package com.yada.gcs.sdk
+package com.yada.sdk.gcs
 
 import javax.jws.{WebService, WebMethod, WebParam, WebResult}
 import javax.xml.bind.annotation.{XmlAccessType, XmlAccessorType, XmlType}
 import javax.xml.ws.{RequestWrapper, ResponseWrapper}
 
 /**
-  * Created by locky on 2016/3/17.
+  * GCS的WSDL文件对应的接口
   */
 @WebService(targetNamespace = "www.boc.com.cn/gcs", name = "Gateway")
 trait Gateway {
   @WebMethod
-  @RequestWrapper(localName = "service", targetNamespace = "www.boc.com.cn/gcs", className = "com.yada.gcs.sdk.Service")
-  @ResponseWrapper(localName = "serviceResponse", targetNamespace = "www.boc.com.cn/gcs", className = "com.yada.gcs.sdk.ServiceResponse")
+  @RequestWrapper(localName = "service", targetNamespace = "www.boc.com.cn/gcs", className = "com.yada.sdk.gcs.Service")
+  @ResponseWrapper(localName = "serviceResponse", targetNamespace = "www.boc.com.cn/gcs", className = "com.yada.sdk.gcs.ServiceResponse")
   @WebResult(name = "response", targetNamespace = "")
   @throws(classOf[GatewayException])
   def service(@WebParam(name = "request", targetNamespace = "") request: String): String
@@ -25,7 +25,7 @@ class Service {
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "serviceResponse", propOrder = Array("response"))
-class ServiceResponse{
+class ServiceResponse {
   var response: String = _
 }
 
---
+++ SystemAdapterService/src/main/scala/com/yada/sdk/gcs/GcsClient.scala
@@ -0,0 +1,29 @@
+package com.yada.sdk.gcs
+
+import java.net.URL
+import javax.xml.namespace.QName
+
+/**
+  * GCS客户端
+  *
+  * @param wsdlDocumentLocation wsdl文件的地址
+  * @param serviceQName         服务名称
+  * @param portQName            端口名称
+  */
+class GcsClient(wsdlDocumentLocation: URL, serviceQName: QName, portQName: QName) {
+  /**
+    * GCS客户端
+    *
+    * @param wsdlDocumentLocation wsdl文件的地址
+    */
+  def this(wsdlDocumentLocation: URL) {
+    this(wsdlDocumentLocation, new QName("www.boc.com.cn/gcs", "GcsGatewayService"), new QName("www.boc.com.cn/gcs", "GatewayPort"))
+  }
+
+  private val service = javax.xml.ws.Service.create(wsdlDocumentLocation, serviceQName)
+  private val port = service.getPort(portQName, classOf[Gateway])
+
+  def send(request: String): String = {
+    port.service(request)
+  }
+}
