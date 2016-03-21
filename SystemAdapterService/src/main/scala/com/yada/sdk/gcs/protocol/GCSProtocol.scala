89676987ae1c7a0861636160f4bb58ce24c921e9
fengming.jiang@bjyada.com <fengming.jiang@bjyada.com>
2016-03-21 11:35:29 +0800

    完善GCS的请求和响应

--- SystemAdapterService/src/main/scala/com/yada/sdk/gcs/protocol/GCSProtocol.scala
+++ SystemAdapterService/src/main/scala/com/yada/sdk/gcs/protocol/GCSProtocol.scala
@@ -1,17 +1,66 @@
 package com.yada.sdk.gcs.protocol
 
+import java.util.Calendar
+
 import com.yada.sdk.gcs.GCSClient
+import com.yada.sdk.gcs.xml.{Page, System, GCS, XmlHandler}
+
+import scala.collection.mutable
 
 /**
   * GCS协议
   */
-trait GCSProtocol[ResponseType <: GCSResp] extends GCSReq {
-  var gcsClient = GCSClient.GLOBAL
+trait GCSProtocol[RespType <: GCSResp] {
+  private val systemProps = mutable.Map.empty[String, String]
+  private val pageProps = mutable.Map.empty[String, String]
+  setSystemProp("transactionCode", transactionCode)
+  // 00 + yyyyMMddHHmmss
+  setSystemProp("transactionNumber", String.format("00%1$tY%1$tm%1$td%1$tH%1$tM%1$tS", Calendar.getInstance.getTime))
+  setSystemProp("transactionSessionId", transactionSessionId)
+  setSystemProp("requestChannelId", requestChannelId)
+  setSystemProp("txnBranchCode", "00003")
+  setSystemProp("txnBankCode", "003")
+  setSystemProp("txnProvinceCode", "")
+  setSystemProp("txnTerminalCode", "")
+  setSystemProp("txnCounterCode", "")
+  setSystemProp("txnUserCode", TxnUserCodeGenerate.get)
 
-  def send: ResponseType = {
+  def xmlHandler = XmlHandler
+
+  def transactionID: String
+
+  def pageKey: String
+
+  def transactionSessionId: String
+
+  def requestChannelId: String
+
+  def transactionCode: String
+
+  def isRequest = true
+
+  def isResponse = false
+
+  def setSystemProp(key: String, value: String): Unit = systemProps += key → value
+
+  def setPageProps(key: String, value: String): Unit = pageProps += key → value
+
+  def reqXML: String = {
+    val gcs = GCS(transactionID, isRequest, isResponse, System(systemProps), Some(Page(pageKey, pageProps, None)))
+    xmlHandler.toXml(gcs)
+  }
+
+  /**
+    * GCS的客户端获取
+    *
+    * @return
+    */
+  protected def gcsClient = GCSClient.GLOBAL
+
+  def send: RespType = {
     val resp = gcsClient.send(reqXML)
     generate(resp)
   }
 
-  protected def generate(xml: String): ResponseType
+  protected def generate(xml: String): RespType
 }
--- SystemAdapterService/src/main/scala/com/yada/sdk/gcs/protocol/GCSResp.scala
+++ SystemAdapterService/src/main/scala/com/yada/sdk/gcs/protocol/GCSResp.scala
@@ -5,23 +5,42 @@ import com.yada.sdk.gcs.xml.XmlHandler
 /**
   * GCS响应报文
   */
-abstract class GCSResp(xml: String) {
+class GCSResp(xml: String) {
 
   val gcs = xmlHandler.fromXml(xml)
-  // 如果不是成功的响应，直接抛出异常
-  if (systemValue("returnCode") != "+GC00000") throw new RuntimeException(s"error response XML...$xml")
+  if (failedThrowException) {
+    // 如果不是成功的响应，直接抛出异常
+    if (systemValue("returnCode") != "+GC00000") throw new RuntimeException(s"error response XML...$xml")
+  }
 
   def xmlHandler = XmlHandler
 
+  /**
+    * 获取响应头的内容
+    *
+    * @param key 内容所对应的标签name的值
+    * @return
+    */
   def systemValue(key: String): String = {
     gcs.system.props.getOrElse(key, "")
   }
 
+  /**
+    * 获取响应正文的内容
+    *
+    * @param key 内容所对应的标签name的值
+    * @return
+    */
   def pageValue(key: String): String = {
     if (gcs.page.isEmpty) throw new RuntimeException("GCS resp page is not exist...")
     gcs.page.get.props.getOrElse(key, "")
   }
 
+  /**
+    * 获取相应正文的Entity列表
+    *
+    * @return List中是Entity，每个Entity的是一个Map，其中key为prop标签name属性的值,value是prop标签value属性的值
+    */
   def pageList: List[Map[String, String]] = {
     if (gcs.page.isEmpty) throw new RuntimeException("GCS resp page is not exist...")
     val list = gcs.page.get.list
@@ -29,6 +48,13 @@ abstract class GCSResp(xml: String) {
     list.get.entities.map(x => x.toMap)
   }
 
+  /**
+    * 获取响应正文的Entity列表
+    *
+    * @param propsToObj entity下的一组prop标签转换成对象的方法，其中props的key为prop标签name属性的值，value是prop标签value属性的值
+    * @tparam T 输出对象的类型
+    * @return
+    */
   def pageListValues[T](propsToObj: Map[String, String] => T): List[T] = {
     val list = pageList
     list.map(values => {
@@ -36,9 +62,17 @@ abstract class GCSResp(xml: String) {
     })
   }
 
+  /**
+    * 获取响应正文中List下Entity的内容
+    *
+    * @param keys 所需要值对应的KEY（在Entity下，prop标签的name属性的值列表）
+    * @return
+    */
   def pageListValues(keys: Array[String]): List[Map[String, String]] = {
     pageListValues(map => {
       keys.map(key => key -> map.getOrElse(key, "")).toMap
     })
   }
+
+  protected def failedThrowException: Boolean = true
 }
