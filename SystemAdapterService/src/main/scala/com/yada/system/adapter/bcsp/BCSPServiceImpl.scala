b4de3d4b2aee37940f8a22b7895a17eef02cd858
fengming.jiang@bjyada.com <fengming.jiang@bjyada.com>
2016-03-24 15:56:42 +0800

    BCSP业务完成

---
+++ SystemAdapterService/src/main/scala/com/yada/system/adapter/bcsp/BCSPServiceImpl.scala
@@ -0,0 +1,33 @@
+package com.yada.system.adapter.bcsp
+
+import com.typesafe.scalalogging.slf4j.Logger
+import com.yada.sdk.bcsp.protocol.impl.T900000000
+import org.slf4j.LoggerFactory
+
+/**
+  * BCSP服务实现
+  */
+class BCSPServiceImpl extends BCSPService {
+  private val log = Logger(LoggerFactory.getLogger(classOf[BCSPServiceImpl]))
+
+  /**
+    * 短信调用
+    *
+    * @param bsnType   BCSP业务类型
+    * @param content   内容
+    * @param handsetNo 手机号
+    * @param sysId     系统ID
+    * @return 返回是否发送成功 true/false
+    */
+  override def sendSMS(bsnType: String, content: String, handsetNo: String, sysId: String): Boolean = {
+    try {
+      val t9req = new T900000000(handsetNo, sysId, bsnType, content)
+      t9req.send
+      true
+    } catch {
+      case e: Exception =>
+        log.error("sending to bcsp has a error...", e)
+        false
+    }
+  }
+}
