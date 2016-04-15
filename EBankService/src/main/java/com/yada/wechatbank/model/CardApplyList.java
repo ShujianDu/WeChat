b8dd1dcf36b2d87e837a15fa5fe4024688bce574
QinQiang <qiang.qin@bjyada.com>
2016-04-15 13:51:59 +0800

    同步接口字段

--- EBankService/src/main/java/com/yada/wechatbank/model/CardApplyList.java
+++ EBankService/src/main/java/com/yada/wechatbank/model/CardApplyList.java
@@ -1,47 +1,31 @@
 package com.yada.wechatbank.model;
 
-import com.yada.wechatbank.base.BaseModel;
-
 import java.util.List;
 
 /**
  * 预约办卡查询返回信息
  * Created by QinQiang on 2016/4/11.
  */
-public class CardApplyList extends BaseModel {
+public class CardApplyList {
 
     // 是否有下一页
-    private Boolean next;
+    private Boolean hasNext;
 
-    private List<CardApply> applyList;
+    private List<CardApply> cardApplies;
 
-    /**
-     * 是否有下一页
-     *
-     * @return true-有 false-没有
-     */
-    public Boolean hasNext() {
-        return next;
+    public Boolean getHasNext() {
+        return hasNext;
     }
 
-    public Boolean getNext() {
-        return next;
+    public void setHasNext(Boolean hasNext) {
+        this.hasNext = hasNext;
     }
 
-    public void setNext(Boolean next) {
-        this.next = next;
+    public List<CardApply> getCardApplies() {
+        return cardApplies;
     }
 
-    public List<CardApply> getApplyList() {
-        return applyList;
-    }
-
-    public void setApplyList(List<CardApply> applyList) {
-        this.applyList = applyList;
-    }
-
-    @Override
-    public String toString() {
-        return "CardApplyList [next=" + next + ", applyList=" + applyList + "]";
+    public void setCardApplies(List<CardApply> cardApplies) {
+        this.cardApplies = cardApplies;
     }
 }
