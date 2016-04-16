a3d32ec015e0f6355dd07ddba20257ed356dbfd2
liangtieluan <liangtieluan123@163.com>
2016-04-16 19:09:43 +0800

    账单摘要新页面及样式,图片

---
+++ EBank/src/main/webapp/WEB-INF/pages/wechatbank_pages/BillingSummary/list.jsp
@@ -0,0 +1,127 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<%@include file="../../base_pages/base.jsp"%>
+<link rel="stylesheet" type="text/css"
+	href="<c:url value="/css/myBill.css"/>" />
+<title>中国银行信用卡</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name=" format-detection" content="telephone=no" />
+<meta name="viewport"
+	content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0">
+<c:set var="path" value="${pageContext.request.contextPath }"></c:set>
+<script type="text/javascript" src="${path }/js/myBill.js"
+	charset="UTF-8"></script>
+</head>
+<body>
+	<div id="myBill_box">
+		<form name="value_debt">
+			<select id="cardNo" name="messagePick" class="control_card">
+				<option value="">请选择下列的银行卡</option>
+				<c:forEach items="${cardList}" var="item" varStatus="status">
+					<option class=""
+						value="${fn:substringAfter(item, ',')}" name="cardNo"
+						onclick="cardChange();">
+						<c:out value="${fn:substring(item, 0, 16)}" />
+					</option>
+				</c:forEach>
+			</select>
+			 <span id="cardNoWarning" style="color: red; font-size: 12px;"></span>
+		</form>
+		<!--选择日期账单显示-->
+		<div class="check_box">
+			<div class="state">
+				<span class="date">账单日期<span id="periodEndDate"></span></span><span
+					class="state_1">已出账单</span>
+			</div>
+			<div class="select_date">
+				<span class="left left1"></span> <span class="right right2"></span>
+				<div class="date_box">
+					<ul>
+						<c:forEach items="${dateList}" var="item" varStatus="status">
+							<c:if test="${status.index==0 }">
+								<li  class="current_mounth" value="${item }"><span
+									class="year">${fn:substring(item,0,4)}</span> <span
+									class="mounth">${fn:substring(item,4,6)}月 </span></li>
+							</c:if>
+							<c:if test="${status.index!=0 }">
+								<li value="${item }"><span class="year">${fn:substring(item,0,4)}</span>
+									<span class="mounth">${fn:substring(item,4,6)}月 </span></li>
+							</c:if>
+						</c:forEach>
+					</ul>
+				</div>
+			</div>
+					<input type="hidden" id="date" value="${dateList[0]}"></input>
+					<span id="dateWarning" style="color: red; font-size: 12px;"></span>
+			<!--显示价格-->
+			<div class="view_value">
+				<div class="value_title"></div>
+				<div class="value_box ">
+					<span class="value_type"><span id="currencyCode5"></span>欠款</span>
+					<input type="text" class="value" id="value1" name="value1"
+						value="" /> <span type="text" class="value"><span
+						id="value1"></span></span> <input type="button" class="check_dt"
+						id="check_dt1" />
+				</div>
+				<div class="value_box ">
+					<span class="value_type"><span id="currencyCode6"></span>欠款</span>
+					<span type="text" class="value"><span id="value2">0.00</span></span>
+					<input type="button" class="check_dt" id="check_dt2" />
+				</div>
+			</div>
+		</div>
+
+
+		<!--还款形式和信息-->
+		<div class="pay_box">
+			<div class="pay_date">
+				到期还款日&nbsp;&nbsp;&nbsp;&nbsp;<span id="pay_date"></span>
+			</div>
+			<div class="pay_value">
+				账单金额&nbsp;&nbsp;&nbsp;&nbsp;
+				<div class="doubleState1">
+					<span id="currencyCode1" class="currencyCode"></span><span
+						id="closingBalance1"></span>
+				</div>
+				<div class="doubleState">
+					<span id="currencyCode2" class="currencyCode"></span><span
+						id="closingBalance2"></span>
+				</div>
+			</div>
+			<div class="lineGray"></div>
+			<div class="pay_value">
+				最低还款额&nbsp;&nbsp;&nbsp;&nbsp;
+				<div class="doubleState1">
+					<span id="currencyCode3" class="currencyCode"></span><span
+						id="minPaymentAmount1"></span>
+				</div>
+				<div class="doubleState">
+					<span id="currencyCode4" class="currencyCode"></span><span
+						id="minPaymentAmount2"></span>
+				</div>
+			</div>
+			<input type="button" value="账单分期" class="stage_btn" /> <input
+				type="button" value="消费分期" class="spend_btn" />
+		</div>
+
+		<!--未出账单-->
+		<a href="#" class="noBill">未出账单</a>
+
+		<!--wait_box -->
+		<div class="wait_box">
+			<div class="tip_box">
+				<img src="images/Loading2.gif" /> <span class="wait_text">loading</span>
+			</div>
+		</div>
+	</div>
+	<script>
+	function cardChange() {
+		messageRevealWait();
+	}
+</script>
+</body>
+</html>
+
---
+++ EBank/src/main/webapp/js/myBill.js
@@ -0,0 +1,139 @@
+	var m=0;
+	//账单日期
+	var date;
+	$(document).ready(function(){
+			$(".check_box .select_date .date_box li").click(function(){
+			$(".check_box .select_date .date_box li").removeClass("current_mounth");
+			$(this).addClass("current_mounth");
+			m=$(this).index();
+			date = $(this).val();
+			messageRevealWait();
+		});
+	});
+	//等待加载的界面显示和隐藏
+	function messageRevealWait(){
+		var cardNo = $("#cardNo").val();
+		var cardNoWarning = $("#cardNoWarning");
+		cardNoWarning.text("");
+		if (cardNo == null || cardNo == "") {
+			cardNoWarning.text("*请选择卡号！");
+            return;
+        }
+		 $.ajax({
+	            url: "getBillingSummary.do",
+	            data: {
+	            	cardNo: cardNo,
+	                date: date
+	            },
+	            type: "post",
+	            dataType: "json",
+	            async: false,
+	            success: function (json) {
+	                if (json != null && json != "") {
+	                	messageReveal(json);
+	                }
+	            }
+	        });
+		}
+	function messageReveal(json) {
+		if(json[0].closingBalance>0){
+			$(".view_value  #value1").parent(".value").children(".valueState").show();
+				$(".view_value  #value1").css("color","#e05d4f");
+				$(".view_value  #value1").css("font-size","22px");
+				$(".view_value  #value1").parent(".value").parent(".value_box").children("#check_dt1").show();
+			document.getElementById("value1").innerText = json[0].closingBalance;
+			}else if(json[0].closingBalance==0){
+				$(".view_value .RMBdebt").show();
+				document.getElementById("value1").innerText="本卡当前月未出"+json[0].currencyCode+"账单";
+				$(".view_value  #value1").parent(".value").children(".valueState").hide();
+				$(".view_value  #value1").css("color","#999999");
+				$(".view_value  #value1").css("font-size","14px");
+				$(".view_value  #value1").parent(".value").parent(".value_box").children("#check_dt1").hide();
+			}else{
+				$(".view_value .RMBdebt").hide();
+				
+			}
+		//分是否为双币单币卡
+		if(json.length>1){
+			$(".view_value").show();
+			$(".value_box").eq(0).show();
+			$("value_box").eq(1).show();
+			$(".pay_box .pay_value  .doubleState1").show();
+			$(".pay_box .pay_value  .doubleState").show();
+		}else if(json.length==1){
+			$(".view_value").show();
+			$(".value_box").eq(0).show();
+			$(".value_box").eq(1).hide();
+			$(".pay_box .pay_value  .doubleState1").show();
+			$(".pay_box .pay_value  .doubleState").hide();
+	
+			}else{
+				$(".view_value").hide();
+				$(".value_box").eq(0).hide();
+				$(".value_box").eq(1).hide();
+				$(".pay_box .pay_value  .doubleState1").hide();
+				$(".pay_box .pay_value  .doubleState").hide();
+				}
+		var n=0;
+		n=document.value_debt.messagePick.selectedIndex;
+		document.getElementById("pay_date").innerText = json[0].paymentDueDate;
+		document.getElementById("periodEndDate").innerText = json[0].periodEndDate;
+		document.getElementById("currencyCode1").innerText = json[0].currencyCode;
+		document.getElementById("currencyCode3").innerText = json[0].currencyCode;
+		document.getElementById("closingBalance1").innerText = json[0].closingBalance;
+		document.getElementById("currencyCode5").innerText = json[0].currencyCode;
+		document.getElementById("minPaymentAmount1").innerText = json[0].minPaymentAmount;
+		document.getElementById("closingBalance2").innerText = json[1].closingBalance;
+		document.getElementById("minPaymentAmount2").innerText = json[1].minPaymentAmount;
+		document.getElementById("currencyCode2").innerText = json[1].currencyCode;
+		document.getElementById("currencyCode4").innerText = json[1].currencyCode;
+		document.getElementById("currencyCode4").innerText = json[1].currencyCode;
+		document.getElementById("currencyCode6").innerText = json[1].currencyCode;
+		if(json[1].closingBalance<0){
+			$(".view_value  #value2").parent(".value").children(".valueState").show();
+				$(".view_value  #value2").css("color","#e05d4f");
+				$(".view_value  #value2").css("font-size","22px");
+				$(".view_value  #value2").parent(".value").parent(".value_box").children("#check_dt2").show();
+				document.getElementById("value2").innerText = json[1].closingBalance;
+			}else if(json[1].closingBalance==0){
+				$(".view_value .dollarDebt").show();
+				document.getElementById("value2").innerText="本卡当前月未出"+json[1].currencyCode+"账单";
+				$(".view_value  #value2").parent(".value").children(".valueState").hide();
+				$(".view_value  #value2").css("color","#999999");
+				$(".view_value  #value2").css("font-size","14px");
+				$(".view_value  #value2").parent(".value").parent(".value_box").children("#check_dt2").hide();
+			}else{
+				$(".view_value .dollarDebt").hide();
+			}
+	}
+	
+$(function(){
+	//选择时间月份
+		var n=0;
+		$(".check_box .select_date .left").click(function(){
+			if(n<$(".check_box .select_date .date_box li").length-8){
+				n=n+4;
+				$(".check_box .select_date .date_box ul").animate({marginRight:-1*$(".check_box .select_date .date_box li").width()*n},1000);
+				$(".check_box .select_date .left").removeClass("left2").addClass("left1");
+				$(".check_box .select_date .right").removeClass("right2").addClass("right1");
+			}else{
+				$(".check_box .select_date .left").removeClass("left1").addClass("left2");
+				$(".check_box .select_date .right").removeClass("right2").addClass("right1");
+				$(".check_box .select_date .date_box ul").animate({marginRight:-1*$(".check_box .select_date .date_box li").width()*8},1000);
+				n=8;
+				}
+		});
+		$(".check_box .select_date .right").click(function(){
+			if(n>4){
+				n=n-4;
+				$(".check_box .select_date .date_box ul").animate({marginRight:-1*$(".check_box .select_date .date_box li").width()*n},1000);
+				$(".check_box .select_date .left").removeClass("left2").addClass("left1");
+				$(".check_box .select_date .right").removeClass("right2").addClass("right");
+			}else{
+				$(".check_box .select_date .left").removeClass("left2").addClass("left1");
+				$(".check_box .select_date .right").removeClass("right1").addClass("right2");
+				$(".check_box .select_date .date_box ul").animate({marginRight:0},1000);
+				n=0;
+				}
+		});
+})
