99d21041086fc0a8226f75be797d828a355b04bb
fengming.jiang@bjyada.com <fengming.jiang@bjyada.com>
2016-04-20 10:43:19 +0800

    完成账单寄送方式的协议测试

--- SystemAdapterService/src/test/scala/com/yada/sdk/gcs/GCSProtocolTest.scala
+++ SystemAdapterService/src/test/scala/com/yada/sdk/gcs/GCSProtocolTest.scala
@@ -1,6 +1,6 @@
 package com.yada.sdk.gcs
 
-import com.yada.sdk.gcs.protocol.impl.TS010002
+import com.yada.sdk.gcs.protocol.impl.{TS010002, TS010056}
 
 /**
   * GCS协议执行测试
@@ -8,14 +8,22 @@ import com.yada.sdk.gcs.protocol.impl.TS010002
 object GCSProtocolTest extends App {
   val sessionID = "93c4399ad67d925fa40d0693adb0a222"
   val channelID = "WX01"
-  //账单寄送方式查询
-  testTS010002()
+  // 账单寄送方式查询
+  //  testTS010002()
+  // 账单寄送方式修改
+  testTS010056()
 
   def testTS010002(): Unit = {
     val cardNo = "5149580068840943"
     val req = new TS010002(sessionID, channelID, cardNo)
     val resp = req.send
     println(resp)
-    // TODO 测试没有完成
+  }
+
+  def testTS010056(): Unit = {
+    val cardNo = "5149580068840943"
+    val req = new TS010056(sessionID, channelID, cardNo, "C")
+    val resp = req.send
+    println(resp)
   }
 }
