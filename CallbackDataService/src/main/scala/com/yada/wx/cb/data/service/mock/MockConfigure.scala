f6ae4a78e3eca678d54d2f90870e2e8d154db9f5
fengming.jiang@bjyada.com <fengming.jiang@bjyada.com>
2016-04-25 18:28:45 +0800

    修改一个无法编译的错误

--- CallbackDataService/src/main/scala/com/yada/wx/cb/data/service/mock/MockConfigure.scala
+++ CallbackDataService/src/main/scala/com/yada/wx/cb/data/service/mock/MockConfigure.scala
@@ -6,7 +6,7 @@ object MockConfigure {
   val bingOpenId = "1111111111111111"
   val unBingOpenId = "2222222222222222"
 
-  val defCustomerInfo = CustomerInfo(,bingOpenId,"2310831118","身份证","20160315","6225880131631111")
+  val defCustomerInfo = CustomerInfo(bingOpenId,"2310831118","身份证","20160315","6225880131631111")
 
   val bindCusts =Map{
     bingOpenId -> defCustomerInfo
