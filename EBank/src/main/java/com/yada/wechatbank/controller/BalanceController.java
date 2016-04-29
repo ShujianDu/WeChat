3eee7b8fbb03e049c435a7ecc30daa1896830165
pangchangsong <changsong.pang@bjyada.com>
2016-04-29 15:46:09 +0800

    额度实体添加toString方法，代码格式化

--- EBank/src/main/java/com/yada/wechatbank/controller/BalanceController.java
+++ EBank/src/main/java/com/yada/wechatbank/controller/BalanceController.java
@@ -21,59 +21,62 @@ import java.util.List;
 
 /**
  * 我的额度
+ *
  * @author tx
  */
 @Controller
 @RequestMapping(value = "balance")
-public class BalanceController extends BaseController{
-	private final Logger logger = LoggerFactory
-			.getLogger(this.getClass());
+public class BalanceController extends BaseController {
+    private final Logger logger = LoggerFactory
+            .getLogger(this.getClass());
 
-	@Autowired
-	private BalanceService balanceServiceImpl;
+    @Autowired
+    private BalanceService balanceServiceImpl;
 
-	public static final String LISTURL = "wechatbank_pages/Balance/list";
+    public static final String LISTURL = "wechatbank_pages/Balance/list";
 
-	/**
-	 * 额度查询
-	 */
-	@RequestMapping(value = "list")
-	public String list(HttpServletRequest request,@ModelAttribute("formBean") BalanceQuery balanceQuery, Model model) {
-		request.getSession().setAttribute("menuId", "2");
-		String identityNo = getIdentityNo(request);
-		String identityType = getGcsIdentityType(request);
-		List<CardInfo> cardList = balanceServiceImpl.getProessCardNoList(identityType, identityNo);
-		if (cardList == null) {
-			logger.info("@Balance根据证件类型[{}]证件号[{}]获取到的卡列表为null", identityType, identityNo);
-			return BUSYURL;
-		} else if (cardList.size() == 0) {
-			logger.info("@Balance根据证件类型[{}]证件号[{}]获取到的卡列表长度为0", identityType, identityNo);
-			return NOCARDURL;
-		} else {
-			logger.info("@Balance根据证件类型[{}]证件号[{}]获取卡片列表,获取到的卡列表cardList[{}]", identityType, identityNo, cardList);
-			model.addAttribute("cardList", cardList);
-			return LISTURL;
-		}
-	}
-	/**
-	 * 额度查询获取单卡数据
-	 * @param cardNo        卡号
-	 */
-	@RequestMapping(value = "getCardNoBalance_Ajax")
-	@ResponseBody
-	public String getCardNoBalance_Ajax(String cardNo) {
-		try {
-			cardNo = Crypt.decode(cardNo);
-		} catch (Exception e) {
-			logger.error("@Balance解密cardNo[{}]失败", cardNo);
-			return JSONObject.toJSONString("exception");
-		}
-		List<Balance> newList = balanceServiceImpl.getCardNoBalance(cardNo);
-		logger.info("@Balance通过cardNo[{}]获取到的额度集合为[{}]", cardNo, newList);
-		if (newList == null) {
-			return JSONObject.toJSONString("exception");
-		}
-		return JSONObject.toJSONString(newList);
-	}
+    /**
+     * 额度查询
+     */
+    @RequestMapping(value = "list")
+    public String list(HttpServletRequest request, @ModelAttribute("formBean") BalanceQuery balanceQuery, Model model) {
+        request.getSession().setAttribute("menuId", "2");
+        String identityNo = getIdentityNo(request);
+        String identityType = getGcsIdentityType(request);
+        List<CardInfo> cardList = balanceServiceImpl.getProessCardNoList(identityType, identityNo);
+        if (cardList == null) {
+            logger.info("@Balance根据证件类型[{}]证件号[{}]获取到的卡列表为null", identityType, identityNo);
+            return BUSYURL;
+        } else if (cardList.size() == 0) {
+            logger.info("@Balance根据证件类型[{}]证件号[{}]获取到的卡列表长度为0", identityType, identityNo);
+            return NOCARDURL;
+        } else {
+            logger.info("@Balance根据证件类型[{}]证件号[{}]获取卡片列表,获取到的卡列表cardList[{}]", identityType, identityNo, cardList);
+            model.addAttribute("cardList", cardList);
+            return LISTURL;
+        }
+    }
+
+    /**
+     * 额度查询获取单卡数据
+     *
+     * @param cardNo 卡号
+     */
+    @RequestMapping(value = "getCardNoBalance_Ajax")
+    @ResponseBody
+    public String getCardNoBalance_Ajax(String cardNo) {
+        try {
+            cardNo = Crypt.decode(cardNo);
+        } catch (Exception e) {
+            logger.error("@Balance解密cardNo[{}]失败", cardNo);
+            return JSONObject.toJSONString("exception");
+        }
+        List<Balance> newList = balanceServiceImpl.getCardNoBalance(cardNo);
+        logger.info("@Balance通过cardNo[{}]获取到的额度集合为[{}]", cardNo, newList);
+        if (newList == null) {
+            return JSONObject.toJSONString("exception");
+        }
+        return JSONObject.toJSONString(newList);
+    }
 }
 
