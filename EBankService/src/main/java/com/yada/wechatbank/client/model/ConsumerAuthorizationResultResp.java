13aaed04875a29f0a2fba247dce4bd1aab68236b
liangtieluan <liangtieluan123@163.com>
2016-04-29 11:33:36 +0800

    修改消费分期授权实体

--- EBankService/src/main/java/com/yada/wechatbank/client/model/ConsumerAuthorizationResultResp.java
+++ EBankService/src/main/java/com/yada/wechatbank/client/model/ConsumerAuthorizationResultResp.java
@@ -1,12 +1,14 @@
 package com.yada.wechatbank.client.model;
 
+import com.yada.wechatbank.base.BaseModel;
+
 /**
  * 消费分期授权行内service返回结果
  * 
  * @author liangtieluan
  *
  */
-public class ConsumerAuthorizationResultResp {
+public class ConsumerAuthorizationResultResp extends BaseModel {
 	private String data;
 
 	public String getData() {
