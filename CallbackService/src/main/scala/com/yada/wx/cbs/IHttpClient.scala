02fa578d0c2f44534f30d5ee31bad927799db376
fengming.jiang@bjyada.com <fengming.jiang@bjyada.com>
2016-05-05 15:13:36 +0800

    处理一些bug

--- CallbackService/src/main/scala/com/yada/wx/cbs/IHttpClient.scala
+++ CallbackService/src/main/scala/com/yada/wx/cbs/IHttpClient.scala
@@ -1,12 +1,9 @@
 package com.yada.wx.cbs
 
-import java.io.{BufferedReader, InputStreamReader}
 import java.net.{HttpURLConnection, URL}
 
 import com.typesafe.config.ConfigFactory
-import com.typesafe.scalalogging.Logger
 
-import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
 
@@ -19,25 +16,22 @@ class HttpClient extends IHttpClient {
   override def send(value: String, url: String): String = {
     new URL(HttpClient.baseURL + url).openConnection() match {
       case hc: HttpURLConnection =>
-        println("===========" + value)
-        hc.setDoInput(true)
-        hc.setDoOutput(true)
-//        hc.setReadTimeout(2000)
-//        hc.setConnectTimeout(2000)
-        hc.getOutputStream.write(value.getBytes("UTF-8"))
-        hc.getOutputStream.flush()
-        val source = Source.fromInputStream(hc.getInputStream)
-        val resp = source.mkString
-        //        val ab = new ArrayBuffer[Byte]()
-        //        val bts = new Array[Byte](1024)
-        //        var len = -1
-        //        do {
-        //          len = hc.getInputStream.read(bts)
-        //        } while (len > 0)
-        //        println("------------" + ab.size)
-        //        ""
-        println("--------" + resp)
-        resp
+        try {
+          println("===========" + value)
+          hc.setDoInput(true)
+          hc.setDoOutput(true)
+          //        hc.setReadTimeout(2000)
+          //        hc.setConnectTimeout(2000)
+          hc.getOutputStream.write(value.getBytes("UTF-8"))
+          hc.getOutputStream.flush()
+          val source = Source.fromInputStream(hc.getInputStream)
+          val resp = source.mkString
+          println("--------" + resp)
+          resp
+        } finally {
+          hc.disconnect()
+        }
+
     }
   }
 }
