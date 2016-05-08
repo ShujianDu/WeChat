a32b2052eedb0a35e0f3cf49b5467851337dda36
fengming.jiang@bjyada.com <fengming.jiang@bjyada.com>
2016-05-08 12:56:27 +0800

    指令查询附近网店完成

--- CallbackService/src/main/scala/com/yada/wx/cbs/subBiz/QueryNearBankBiz.scala
+++ CallbackService/src/main/scala/com/yada/wx/cbs/subBiz/QueryNearBankBiz.scala
@@ -1,13 +1,22 @@
 package com.yada.wx.cbs.subBiz
 
-import com.yada.wx.cb.data.service.jpa.model.{Command, Customer}
+import com.yada.wx.cb.data.service.SpringContext
+import com.yada.wx.cb.data.service.jpa.dao.{MsgComDao, NewsComDao}
+import com.yada.wx.cb.data.service.jpa.model.{Command, Customer, MsgCom, NewsCom}
 import com.yada.wx.cbs.{CmdRespMessage, ICmdSubBiz}
+
+import scala.collection.convert.WrapAsScala
 
 /**
   * 查询附近网点
   */
-class QueryNearBankBiz extends ICmdSubBiz {
+class QueryNearBankBiz(msgComDao: MsgComDao = SpringContext.context.getBean(classOf[MsgComDao]),
+                       newsComDao: NewsComDao = SpringContext.context.getBean(classOf[NewsComDao])) extends ICmdSubBiz {
   override def subHandle(command: Command, customer: Customer): CmdRespMessage = {
-    ???
+    val findMsgCom: () => MsgCom = () => msgComDao.findOne(command.success_msg_id)
+    val findNewsCom: String => List[NewsCom] = msgID => WrapAsScala.asScalaBuffer(newsComDao.findByMsgID(msgID)).toList
+    val np: String => String = t => t
+    val rp: String => List[String] = t => List(t)
+    createRespMsg(findMsgCom, findNewsCom, np, rp)
   }
 }
