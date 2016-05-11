0483656e3f6b06dd3fddd6eec961cd542149bdfa
zhangman <man.zhang@bjyada.com>
2016-05-11 19:58:17 +0800

    格式化代码

--- EBankService/src/main/java/com/yada/wechatbank/service/PointsService.java
+++ EBankService/src/main/java/com/yada/wechatbank/service/PointsService.java
@@ -8,14 +8,14 @@ import com.yada.wechatbank.model.VerificationCardNoResult;
 import java.util.List;
 
 /**
- * Created by Echo on 2016/4/12.
+ * @author zm
  */
 public interface PointsService {
 
     /**
      * 获取账户积分余额
      *
-     * @return
+     * @return 积分余额
      */
     PointsBalance getPointsBlance(String identityType, String identityNo);
 
@@ -32,7 +32,7 @@ public interface PointsService {
      * 处理远程接口返回的积分明细集合
      *
      * @param list 积分明细列表
-     * @return List<List<JifenDetail>>
+     * @return List List<JifenDetail>
      */
     List<List<PointsDetail>> getList(List<PointsDetail> list);
 
@@ -47,17 +47,17 @@ public interface PointsService {
     /**
      * 获取客户卡号
      *
-     * @param identityType
-     * @param identityNo
-     * @return
+     * @param identityType 证件类型
+     * @param identityNo 证件号
+     * @return 卡号
      */
     String getCardNo(String identityType, String identityNo);
 
     /**
      * 通过卡号获取加密后的卡号和sign
      *
-     * @param cardNo
-     * @return
+     * @param cardNo 卡号
+     * @return 加密结果
      */
     VerificationCardNoResult verificationCardNo(String cardNo);
 }
