53f22af7c5669fbdc8c0fd08f7594bd2243eadbe
zqd <190167177@qq.com>
2016-05-11 15:56:18 +0800

    积分定死的规则修改

--- Encrypt/src/main/java/com/yada/PointDes.java
+++ Encrypt/src/main/java/com/yada/PointDes.java
@@ -1,21 +1,20 @@
 package com.yada;
 
-import java.util.Random;
 
 /**
  * 积分加密/解密
  */
 public class PointDes {
-    private char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+//    private char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
     private Des des = new Des();
-    private Random random = new Random();
+//    private Random random = new Random();
 
     public String encrypt(String data, String key) {
-        char[] cs = new char[6];
-        for (int i = 0; i < 6; i++) {
-            cs[i] = chars[random.nextInt(26)];
-        }
-        return des.encrypt(String.format("%s_%s_%s", new String(cs, 0, 3), data, new String(cs, 3, 3)), key);
+//        char[] cs = new char[6];
+//        for (int i = 0; i < 6; i++) {
+//            cs[i] = chars[random.nextInt(26)];
+//        }
+        return des.encrypt(String.format("aaa_%s_vvv", data), key);
     }
 
     public String decrypt(String data, String key) {
